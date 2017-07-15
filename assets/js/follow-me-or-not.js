af47997763a96d26ac22be3abda24361c4d596e1
Jakub Niewiarowski <jakub@niewiarowski.it>
2017-07-15 19:28:55 +0200

    Firefox compatibility.

--- assets/js/follow-me-or-not.js
+++ assets/js/follow-me-or-not.js
@@ -5,14 +5,14 @@
 * @Last Modified time: 2016-09-17 11:53:32
 */
 
-$(document).ready(function(){
+$(function(){
 	var username_a = $('meta[name=user-login]').attr("content").trim();
 	var username_b = $("meta[property='profile:username']").attr("content").trim();
 	var url = "https://api.github.com/users/" + username_b + "/following/" + username_a;
 	var new_text = username_b + "<br><span class='text-muted text-small'>FOLLOWS YOU</span>";
 
-	// if "username_a" is empty, user is not logged in 
-	// is "username_b" is empty, it is not user page 
+	// if "username_a" is empty, user is not logged in
+	// is "username_b" is empty, it is not user page
 	if(username_a != "" && username_b != "" && username_a != username_b){
 		$.ajax({
 			url : url,
