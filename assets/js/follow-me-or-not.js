e205dc78919e5fff5161ee0c7a8d2ee882723162
Jakub Niewiarowski <jakub@niewiarowski.it>
2017-07-16 05:10:36 +0200

    pjax page update support.

--- assets/js/follow-me-or-not.js
+++ assets/js/follow-me-or-not.js
@@ -6,19 +6,25 @@
 */
 
 $(function(){
-	var username_a = $('meta[name=user-login]').attr("content").trim();
-	var username_b = $("meta[property='profile:username']").attr("content").trim();
-	var url = "https://api.github.com/users/" + username_b + "/following/" + username_a;
-	var new_text = username_b + "<br><span class='text-muted text-small'>FOLLOWS YOU</span>";
+	function process() {
+		var username_a = $('meta[name=user-login]').attr("content").trim();
+		var username_b = $("meta[property='profile:username']").attr("content").trim();
+		var url = "https://api.github.com/users/" + username_b + "/following/" + username_a;
+		var new_text = username_b + "<br><span class='text-muted text-small'>FOLLOWS YOU</span>";
 
-	// if "username_a" is empty, user is not logged in
-	// is "username_b" is empty, it is not user page
-	if(username_a != "" && username_b != "" && username_a != username_b){
-		$.ajax({
-			url : url,
-			success : function(data){
-				$("span.vcard-username").html(new_text);
-			},
-		});
+		// if "username_a" is empty, user is not logged in
+		// is "username_b" is empty, it is not user page
+		if(username_a != "" && username_b != "" && username_a != username_b){
+			$.ajax({
+				url : url,
+				success : function(data){
+					$("span.vcard-username").html(new_text);
+				},
+			});
+		}
 	}
+
+	process();
+
+	$(document).on('pjax:success', process);
 });
